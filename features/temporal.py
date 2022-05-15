a2f53bba02dba69670f52a876e450629ecf783a5
StratosGrig <eustratios13@gmail.com>
2022-05-15 12:26:03 +0300

    Add temporal feature

--- features/temporal.py
+++ features/temporal.py
@@ -20,3 +20,12 @@ def f_total_hours(data:data):
     last = datetime.strptime(tweets[-1]['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
     hours = (first - last).total_seconds()
     return hours
+
+def get_consecutive_days(data:data): 
+    tweets = data.getTweets()
+    last = datetime.strptime(tweets[-1]['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+    dateList = []
+    numofDays = f_total_days(tweets)
+    for n in range(numofDays + 1):
+        dateList.append(last.date() + timedelta(n))
+    return dateList
