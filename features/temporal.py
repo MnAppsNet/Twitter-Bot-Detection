d334623c87d394113c4d8d4974930625e8695f78
Dimitrah <chatziarad@gmail.com>
2022-05-18 17:03:12 +0300

    added temporal features

--- features/temporal.py
+++ features/temporal.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
 from tools import data
+from collections import Counter
+from scipy import stats
+from features.tools import get_statistical_results_of_list
 
 ################################################################
 # In this script we put all the temporal related features.
@@ -29,3 +32,106 @@ def f_consecutive_days(data:data):
     for n in range(numofDays + 1):
         dateList.append(last.date() + timedelta(n))
     return dateList
+
+def f_get_max_min_tweets_per_day(data:data):    
+    tweets = data.getTweets()
+    dates = []
+    for t in tweets:
+        tweet_date = datetime.strptime(t['created_at'],'%a %b %d %H:%M:%S +0000 %Y').date()
+        dates.append(tweet_date)
+    date_list = f_consecutive_days(tweets)
+    # print (date_list)
+    c = Counter(dates)
+    if len(date_list)>0:
+        for d in date_list:
+            if d not in c:
+                c[d]=0
+        # print (c.values())
+        return min(c.values()), max(c.values()), np.mean(list(c.values())), np.median(list(c.values())), np.std(list(c.values())), \
+               stats.skew(list(c.values())), stats.kurtosis(list(c.values())), stats.entropy(list(c.values())),c
+    else:
+        # print('get_max_min_tweets_per_day')
+        return 0,0,0,0,0,0,0,0,c    
+
+def f_get_consecutive_days_of_no_activity(data:data):
+    tweets = data.getTweets()
+    minV, maxV, meanV, medianV, stdV, \
+    skewV, kurtV, entV, c= f_get_max_min_tweets_per_day(tweets)
+    alldates = f_consecutive_days(tweets)
+    i = 0
+    maximum = 0
+    dat = []
+    if len(alldates) > 0:
+        for d in alldates:
+           if c[d]==0:
+               dat.append(d)
+               i+=1
+           else:
+               if i > maximum:
+                   maximum = i
+               i=0
+        if maximum == 0:
+            maximum = i
+        # print ('maximun days of no activity',maximum)
+        # print (dat)
+        return maximum
+    else:
+        return 0    
+
+def f_get_consecutive_days_of_activity(data:data):
+    tweets = data.getTweets()
+    # print ('get_consecutive_days_of_activity')
+    minV, maxV, meanV, medianV, stdV, \
+    skewV, kurtV, entV, c = f_get_max_min_tweets_per_day(tweets)
+    alldates = f_consecutive_days(tweets)
+    i = 0
+    maximum = 0
+    dat = []
+    if len(alldates) > 0:
+        for d in alldates:
+           if c[d]>0:
+               dat.append(d)
+               i+=1
+           else:
+               if i > maximum:
+                   maximum = i
+               i=0
+        if maximum == 0:
+            maximum = i
+        # print ('maximum days of activity',maximum)
+        # print (dat)
+        return maximum
+    else:
+        return maximum           
+
+def f_get_average_time_between_tweets(data:data):
+    tweets = data.getTweets()    
+    # print ('get average time between tweets')
+    gapList = []
+    for i in range(0, len(tweets) - 1):
+        first = datetime.strptime(tweets[i]['created_at'],
+                                  '%a %b %d %H:%M:%S +0000 %Y')
+        second = datetime.strptime(tweets[i + 1]['created_at'],
+                                   '%a %b %d %H:%M:%S +0000 %Y')
+        gap = ((first - second).seconds)
+        gapList.append(gap)
+    return get_statistical_results_of_list(gapList)      
+
+def f_get_max_occurence_of_same_gap(data:data):
+    tweets = data.getTweets()     
+    # print ('get average time between tweets')
+    gapList = []
+    if len(tweets) > 2:
+        for i in range(0, len(tweets) - 1):
+            first = datetime.strptime(tweets[i]['created_at'],
+                                      '%a %b %d %H:%M:%S +0000 %Y')
+            second = datetime.strptime(tweets[i + 1]['created_at'],
+                                       '%a %b %d %H:%M:%S +0000 %Y')
+            gap = ((first - second).seconds)
+            gapList.append(gap)
+        c = Counter(gapList)
+        # print (gapList)
+        max_occ = c.most_common(1)[0][1]
+    else:
+        max_occ = 0
+    return max_occ       
