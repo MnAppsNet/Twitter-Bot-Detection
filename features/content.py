164f0b7c8ca23ffbe83a566fc043cf6b8a49b0ee
Dimitrah <chatziarad@gmail.com>
2022-05-23 10:34:45 +0300

    remove preprocessesor

--- features/content.py
+++ features/content.py
@@ -1,7 +1,6 @@
 import math
 from features.tools import data
 from features.tools import get_all_texts, get_statistical_results_of_list
-from tweeter_preprocessing import TwitterPreprocessor
 from itertools import combinations
 from nltk import pos_tag, word_tokenize
 import string
@@ -25,50 +24,6 @@ from collections import Counter
 #and the date_last.
 ################################################################
 
-def calculate_entropy(sentence):
-    entropy = 0
-    # There are 256 possible ASCII characters
-    for character_i in range(256):
-        Px = sentence.count(chr(character_i)) / len(sentence)
-        if Px > 0:
-            entropy += - Px * math.log(Px, 2)
-    return entropy
-
-def f_text_size_distributions(data:data):
-    tweets = data.getTweets()
-    length=[]
-    for t in tweets:
-        if 'retweeted_status' not in t:
-            try:
-                text = t['full_text']
-            except KeyError:
-                text = t['text']
-            p = TwitterPreprocessor(text)
-            p.partially_preprocess()
-            new = p.text
-            numOfWords = len(new.split())
-            length.append(numOfWords)
-    return get_statistical_results_of_list(length)
-
-def f_text_entropy_distributions(data:data):
-    tweets = data.getTweets()
-    texts = []
-    for t in tweets:
-        if 'retweeted_status' not in t:
-            try:
-                text = t['full_text']
-            except KeyError:
-                text = t['text']
-            p = TwitterPreprocessor(text)
-            p.partially_preprocess()
-            new = p.text
-            texts.append(text)
-    entropies= []
-    for t in texts:
-        ent = calculate_entropy(t)
-        entropies.append(ent)
-    return get_statistical_results_of_list(entropies)
-
 def f_jaccard_sim(str1, str2):
     a = set(str1.split())
     b = set(str2.split())
@@ -87,32 +42,6 @@ def f_similarities(data:data):
         for c in combos:
             similarities.append(f_jaccard_sim(c[0], c[1]))
     return get_statistical_results_of_list(similarities)
-
-def f_total_pos_tags(data:data):
-    tweets = data.getTweets()
-    texts = get_all_texts(tweets)
-    allTags=[]
-    for t in texts:
-        p = TwitterPreprocessor(t)
-        p.pos_tag_preprocess()
-        new = p.text
-        text = word_tokenize(new)
-        tags = [i[1] for i in pos_tag(text)]
-        allTags.extend(tags)
-    return allTags
-
-def f_pos_tag_per_tweet(data:data):
-    tweets = data.getTweets()
-    texts = get_all_texts(tweets)
-    perTweetTags=[]
-    for t in texts:
-        p = TwitterPreprocessor(t)
-        p.pos_tag_preprocess()
-        new = p.text
-        text = word_tokenize(new)
-        tags = [i[1] for i in pos_tag(text)]
-        perTweetTags.append(tags)
-    return perTweetTags
 
 def f_all_punctuation_marks(data:data):
     tweets = data.getTweets()
@@ -158,7 +87,7 @@ def get_tweet_retweet_ratio(data:data):
     if rts == 0:
         rts += 1
     ratio = ts / (rts)
-    return ts, rts, ratio
+    return ratio
 
 def source_change(data:data):
     tweets = data.getTweets()
