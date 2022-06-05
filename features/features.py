920068071ad2f59f61f202b883557802fd879744
Kalyvas Emmanouil <emkalyvas@mnapps.net>
2022-06-05 13:05:49 +0300

    Fix issue with get_all_features method

--- features/features.py
+++ features/features.py
@@ -50,7 +50,7 @@ class Features:
     def get_all_features(self):
         return {
             **self.get_user_features(),
-            **self.get_temporal_features()
+            **self.get_temporal_features(),
             **self.get_sentiment_features(),
             **self.get_content_features(),
             **self.get_network_features()
