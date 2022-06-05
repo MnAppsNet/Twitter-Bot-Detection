aa3ce5cc157862459254250e24e43b80ff4c17e1
Kalyvas Emmanouil <emkalyvas@mnapps.net>
2022-06-05 17:21:36 +0300

    Updaet ml.py

--- ml.py
+++ ml.py
@@ -1,23 +1,18 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split , KFold
-from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from pandas import DataFrame
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 #from xgboost import XGBClassifier
 from sklearn import model_selection
-from sklearn.utils import class_weight
+from sklearn import metrics
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
 
@@ -25,49 +20,63 @@ import pandas as pd
 def splitData(X:DataFrame,Y:DataFrame):
     return train_test_split(X,Y,train_size=0.7,random_state=42)
 
-def randomForest(dataset:DataFrame):
-    estimators = 100
-    criterion = 'gini'
-    max_depth = 10
-    min_samples_split = 2
-    Y = dataset.label.copy()
-    X = dataset.copy()
-    X = X.drop(columns=['label'])
-    x_train, x_test, y_train, y_test = splitData(X,Y)
-    model = RandomForestClassifier(estimators, criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split, random_state=42)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
-    return model, {
-        'accuracy':metrics.accuracy_score(y_test,y_pred),
-        'precision':metrics.precision_score(y_test,y_pred),
-        'recall':metrics.recall_score(y_test,y_pred),
-        'f1':metrics.f1_score(y_test,y_pred)
-    }
-
 def trainModels(dataset:DataFrame):
+    #Split features and label :
     Y = dataset.label.copy()
     X = dataset.copy()
     X = X.drop(columns=['label'])
-    x_train, x_test, y_train, y_test = splitData(X,Y)
+
+    #Scale features :
+    X = MinMaxScaler().fit_transform(X)
 
     models = [
-          ('LogReg', LogisticRegression(),{}),
-          ('RF', RandomForestClassifier(n_estimators=100,criterion='gini',max_depth=10,min_samples_split=2),{}),
-          ('KNN', KNeighborsClassifier(),{}),
-          ('SVM', SVC(),{}),
-          ('GNB', GaussianNB(),{}),
+          ('RF', RandomForestClassifier(n_estimators=100,criterion='gini',max_depth=10,min_samples_split=2)),
+          ('KNN', KNeighborsClassifier()),
+          ('SVM', SVC()),
+          ('GNB', GaussianNB()),
          # ('XGB', XGBClassifier())
         ]
 
+    results = []
+
     scoring = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted', 'roc_auc']
     target_names = ['BOT', 'HUMAN']
-    for name, model, metrics in models:
+    for name, model in models:
+        result = {}
+        result['name'] = name;
+        result['model'] = model;
         kfold = model_selection.KFold(n_splits=5, shuffle=True, random_state=42)
-        cv_results = model_selection.cross_validate(model, x_train, y_train, cv=kfold, scoring=scoring)
-        clf = model.fit(x_train, y_train)
-        y_pred = clf.predict(x_test)
-        print(name)
-        metrics = classification_report(y_test, y_pred, target_names=target_names)
-        print(metrics)
+        cv_results = model_selection.cross_validate(model, X, Y, cv=kfold, scoring=scoring)
+        result['metrics'] = {}
+        for s in scoring:
+            result['metrics'][s] = np.mean(cv_results['test_' + s])
+        full_results = pd.DataFrame(cv_results)
+        full_results['model'] = name
+        result['full_results'] = full_results.copy()
+        results.append(result)
+    return results
 
-    return models
+def modelComparison(results):
+    final = pd.concat([res['full_results'] for res in results], ignore_index=True)
+    bootstraps = []
+    for model in list(set(final.model.values)):
+        model_df = final.loc[final.model == model]
+        bootstrap = model_df.sample(n=30, replace=True)
+        bootstraps.append(bootstrap)
+    bootstrap_df = pd.concat(bootstraps, ignore_index=True)
+    results_long = pd.melt(bootstrap_df,id_vars=['model'],var_name='metrics', value_name='values')
+    time_metrics = ['fit_time','score_time'] # fit time metrics
+    ## PERFORMANCE METRICS
+    results_long_nofit = results_long.loc[~results_long['metrics'].isin(time_metrics)] # get df without fit data
+    results_long_nofit = results_long_nofit.sort_values(by='values')
+    ## TIME METRICS
+    results_long_fit = results_long.loc[results_long['metrics'].isin(time_metrics)] # df with fit data
+    results_long_fit = results_long_fit.sort_values(by='values')
+    plt.figure(figsize=(20, 12))
+    sns.set(font_scale=2.5)
+    g = sns.boxplot(x="model", y="values", hue="metrics", data=results_long_nofit, palette="Set3")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.title('Comparison of Model by Classification Metric')
+    plt.savefig('./benchmark_models_performance.png',dpi=300)
+    metrics = list(set(results_long_nofit.metrics.values))
+    bootstrap_df.groupby(['model'])[metrics].agg([np.std, np.mean])
